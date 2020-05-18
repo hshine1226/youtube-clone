47cc1727977aeb7955d301b826c1ea52f4a01c7d
hshine1226 <hshine1226@gmail.com>
2020-05-18 19:07:44 +0900

    기능: 미들웨어 추가

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* require를 통해 express를 불러온다. node_module/express */
 // const express = require("express");
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 // app이라는 변수에 express를 실행해서 application을 만든다.
 const app = express();
 
@@ -13,15 +17,13 @@ const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile.");
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm betweenHome");
-  next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
