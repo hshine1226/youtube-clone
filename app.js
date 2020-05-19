7bcc7f49936ee21702de88c39574348b28122b20
hshine1226 <hshine1226@gmail.com>
2020-05-19 21:06:29 +0900

    MVC 패턴을 따르기 위해서 routes 분리

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-/* require를 통해 express를 불러온다. node_module/express */
-// const express = require("express");
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-// default로 export하지 않았기 때문에 이런식으로 불러온다.
-import { userRouter } from "./router";
-// app이라는 변수에 express를 실행해서 application을 만든다.
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
+
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from my ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile.");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -20,9 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
