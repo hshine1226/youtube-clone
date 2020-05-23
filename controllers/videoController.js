bc48b43ad1b1b28c5bd3b8840c9ada2bee01fe31
hshine1226 <hshine1226@gmail.com>
2020-05-23 20:18:49 +0900

    기능: template varialbes를 이용해 varialbes 넘겨주는 기능 추가

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,6 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
 export const serach = (req, res) =>
   res.render("search", { pageTitle: "Search" });
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" });
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) =>
