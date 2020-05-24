600dbaa3f85a20448db2a102ed832ca3be890439
hshine1226 <hshine1226@gmail.com>
2020-05-24 18:35:18 +0900

    형식: html form 화면 구성

---
+++ db.js
@@ -0,0 +1,62 @@
+export const videos = [
+  {
+    id: 32345,
+    title: "Viedo Awesome",
+    description: "This is somthing I love",
+    views: 24,
+    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    creater: {
+      id: 11252,
+      name: "Junhyuk",
+      email: "hshine1226@gmail.com",
+    },
+  },
+  {
+    id: 34533,
+    title: "Viedo Amazing",
+    description: "This is somthing I love",
+    views: 24,
+    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    creater: {
+      id: 11252,
+      name: "Junhyuk",
+      email: "hshine1226@gmail.com",
+    },
+  },
+  {
+    id: 12351,
+    title: "Viedo Super",
+    description: "This is somthing I love",
+    views: 24,
+    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    creater: {
+      id: 11252,
+      name: "Junhyuk",
+      email: "hshine1226@gmail.com",
+    },
+  },
+  {
+    id: 98273,
+    title: "Viedo Cool",
+    description: "This is somthing I love",
+    views: 24,
+    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    creater: {
+      id: 11252,
+      name: "Junhyuk",
+      email: "hshine1226@gmail.com",
+    },
+  },
+  {
+    id: 69828,
+    title: "Viedo Nice",
+    description: "This is somthing I love",
+    views: 24,
+    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    creater: {
+      id: 11252,
+      name: "Junhyuk",
+      email: "hshine1226@gmail.com",
+    },
+  },
+];
