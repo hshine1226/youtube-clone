30b45a9bb27dc34537894df8999ac9e65ab2969c
hshine1226 <hshine1226@gmail.com>
2020-05-24 19:40:20 +0900

    기능: mixins를 이용해서 video block 사용

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ export const videos = [
     title: "Viedo Awesome",
     description: "This is somthing I love",
     views: 24,
-    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    videoFile:
+      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
     creater: {
       id: 11252,
       name: "Junhyuk",
@@ -16,7 +17,8 @@ export const videos = [
     title: "Viedo Amazing",
     description: "This is somthing I love",
     views: 24,
-    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    videoFile:
+      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
     creater: {
       id: 11252,
       name: "Junhyuk",
@@ -28,7 +30,8 @@ export const videos = [
     title: "Viedo Super",
     description: "This is somthing I love",
     views: 24,
-    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    videoFile:
+      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
     creater: {
       id: 11252,
       name: "Junhyuk",
@@ -40,7 +43,8 @@ export const videos = [
     title: "Viedo Cool",
     description: "This is somthing I love",
     views: 24,
-    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    videoFile:
+      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
     creater: {
       id: 11252,
       name: "Junhyuk",
@@ -52,7 +56,8 @@ export const videos = [
     title: "Viedo Nice",
     description: "This is somthing I love",
     views: 24,
-    viedoFile: "https://youtu.be/8h3OCbx7ln8",
+    videoFile:
+      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
     creater: {
       id: 11252,
       name: "Junhyuk",
