1089a55b79b28617c475ec0dd8ab3823baa4c95d
hshine1226 <hshine1226@gmail.com>
2020-05-25 19:28:35 +0900

    로그아웃 컨트롤러 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,8 +29,11 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Logout" });
+export const logout = (req, res) => {
+  // To do: Process Log Out
+  res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const changePassword = (req, res) =>
