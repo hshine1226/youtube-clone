a5878b8c53bc74048b3c1ab79bb8c9768ae5ca6b
hshine1226 <hshine1226@gmail.com>
2020-05-25 15:00:07 +0900

    형식: Join, Login의 컨트롤러를 분리
    각각의 template 에 mixin을 통해 재활용 코드를 이용한다.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,34 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400); // 잘못된 요청
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = (req, res) => {
+  const {
+    body: { email, password },
+  } = req;
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
 export const userDetail = (req, res) =>
