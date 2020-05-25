a6941ba5456d3830f38708d9d8364af345bfedfb
hshine1226 <hshine1226@gmail.com>
2020-05-25 14:58:13 +0900

    형식: isAuthentication에 따라서 header에 나타나는 템플릿 변경

--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,10 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "YouTube";
   res.locals.routes = routes;
+  // db 생기기 전까지 쓰일 가짜 정보
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1,
+  };
   next();
 };
