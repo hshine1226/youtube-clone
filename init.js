f0741f924be44acb2cefc13e9be283b23c5ada11
hshine1226 <hshine1226@gmail.com>
2020-05-27 21:43:26 +0900

    기능: Multer를 통한 URL path 반환

--- init.js
+++ init.js
@@ -2,7 +2,8 @@ import "./db";
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
-import Video from "./models/Video";
+import "./models/Video";
+import "./models/Comment";
 
 const PORT = process.env.PORT || 4000;
 
