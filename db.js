72818d4e0b2155df9aaee5bd349cd429587179fa
hshine1226 <hshine1226@gmail.com>
2020-05-27 17:17:58 +0900

    기능: MongoDB 연결

--- db.js
+++ db.js
@@ -1,67 +1,16 @@
-export const videos = [
-  {
-    id: 32345,
-    title: "Viedo Awesome",
-    description: "This is somthing I love",
-    views: 24,
-    videoFile:
-      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
-    creater: {
-      id: 11252,
-      name: "Junhyuk",
-      email: "hshine1226@gmail.com",
-    },
-  },
-  {
-    id: 34533,
-    title: "Viedo Amazing",
-    description: "This is somthing I love",
-    views: 24,
-    videoFile:
-      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
-    creater: {
-      id: 11252,
-      name: "Junhyuk",
-      email: "hshine1226@gmail.com",
-    },
-  },
-  {
-    id: 12351,
-    title: "Viedo Super",
-    description: "This is somthing I love",
-    views: 24,
-    videoFile:
-      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
-    creater: {
-      id: 11252,
-      name: "Junhyuk",
-      email: "hshine1226@gmail.com",
-    },
-  },
-  {
-    id: 98273,
-    title: "Viedo Cool",
-    description: "This is somthing I love",
-    views: 24,
-    videoFile:
-      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
-    creater: {
-      id: 11252,
-      name: "Junhyuk",
-      email: "hshine1226@gmail.com",
-    },
-  },
-  {
-    id: 69828,
-    title: "Viedo Nice",
-    description: "This is somthing I love",
-    views: 24,
-    videoFile:
-      "https://scontent-gmp1-1.cdninstagram.com/v/t50.2886-16/69620602_355876595296214_2367228161721821585_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkIiwicWVfZ3JvdXBzIjoiW1wiaWdfd2ViX2RlbGl2ZXJ5X3Z0c19vdGZcIl0ifQ&_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=RPpsBgj-qNoAX-_JKlx&vs=17904844135357007_559987673&_nc_vs=HBksFQAYJEdIcFRKZ1RXNXd2NnFrTUJBSkhwdXBGNEZkb2dia1lMQUFBRhUAAsgBABUAGCRHSVh2SlFRQkd4S3ZDNFVIQU4yMC1NSDVMdEUtYmtZTEFBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMBUAABgAFp6H8N6Kl84%2FFQIoAkMzLBdAQgzMzMzMzRgSZGFzaF9iYXNlbGluZV8xX3YxEQB16gcA&_nc_rid=7582575938&oe=5ECC4568&oh=657596865e431ae25b7fb8460a2974df",
-    creater: {
-      id: 11252,
-      name: "Junhyuk",
-      email: "hshine1226@gmail.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+// MongoDB port: 27017(Mongod실행 후 확인)
+mongoose.connect("mongodb://localhost:27017/youtube-clone", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
