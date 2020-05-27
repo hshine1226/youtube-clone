7295ec30d66bb8fe88ea95fc8e5bc2fdaecd2ab2
hshine1226 <hshine1226@gmail.com>
2020-05-27 18:18:13 +0900

    dotenv 환경변수 추가

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 // MongoDB port: 27017(Mongod실행 후 확인)
-mongoose.connect("mongodb://localhost:27017/youtube-clone", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
