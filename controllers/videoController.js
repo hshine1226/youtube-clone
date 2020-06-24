4ad7e64c5bd05a6b0c6c5bcbd6c417d074234a79
hshine1226 <hshine1226@gmail.com>
2020-06-24 23:15:05 +0900

    Set populate comments

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   try {
@@ -53,8 +54,10 @@ export const videoDetail = async (req, res) => {
   } = req;
 
   try {
-    const video = await Video.findById(id).populate("creator");
-    // console.log(video);
+    const video = await Video.findById(id)
+      .populate("creator")
+      .populate("comments ");
+    console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (err) {
     res.redirect(routes.home);
@@ -124,6 +127,30 @@ export const postRegisterView = async (req, res) => {
   } catch (error) {
     res.status(400);
     res.end();
+  } finally {
+    res.end();
+  }
+};
+
+// Add Comment
+
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user,
+  } = req;
+
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id,
+    });
+    video.comments.push(newComment.id);
+    video.save();
+  } catch (error) {
+    res.status(400);
   } finally {
     res.end();
   }
