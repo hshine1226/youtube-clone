013d4f62ee39c56ddf79d404f6a6585f4a11b242
hshine1226 <hshine1226@gmail.com>
2020-07-15 20:29:57 +0900

    Add checking production or no production

--- src/db.js
+++ src/db.js
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // MongoDB port: 27017(Mongod실행 후 확인)
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
